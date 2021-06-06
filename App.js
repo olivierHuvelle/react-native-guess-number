c56ad9fbb0ff961b225b96fbd73c382d8da866e4
Olivier Huvelle <olivier.huvelle@gmail.com>
2021-06-06 21:18:48 +0200

    added screen StartGameScreen component

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
 
-import Header from "./components/UI/Header"
+import Header from './components/UI/Header'
+import StartGameScreen from './screens/StartGameScreen'
 
 export default function App() {
   return (
     <View style={styles.screen}>
       <Header title="Guess a number app" />
+      <StartGameScreen />
     </View>
   );
 }
